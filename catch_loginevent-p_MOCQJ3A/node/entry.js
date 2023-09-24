5020f481139e56bd0c7898cdf36c09648752ad5d
chris <chris@rwap.app>
2023-09-24 12:13:36 +0000

    commit before merging to production

---
+++ catch_loginevent-p_MOCQJ3A/node/entry.js
@@ -0,0 +1,26 @@
+// To use previous step data, pass the `steps` object to the run() function
+import { Client } from "@xmtp/xmtp-js";
+import { Wallet } from "ethers";
+
+export default defineComponent({
+  async run({ steps, $ }) {
+    // You'll want to replace this with a wallet from your application
+    const wallet = Wallet.createRandom();
+    // Create the client with your wallet. This will connect to the XMTP development network by default
+    const xmtp = await Client.create(wallet, { env: "production" });
+    // Start a conversation with XMTP
+    const conversation = await xmtp.conversations.newConversation(
+      "0x44295F9F7157f3078B91f5aed8521F167D725026",
+    );
+    // Load all messages in the conversation
+    const messages = await conversation.messages();
+    // Send a message
+    await conversation.send("LOBBY VISIT: Please check zoom");
+    // // Listen for new messages in the conversation
+    // for await (const message of await conversation.streamMessages()) {
+    //   console.log(`[${message.senderAddress}]: ${message.content}`);
+    // }
+    // Return data to use it in future steps
+    return steps.trigger.event
+  },
+})
